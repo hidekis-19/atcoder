fb9e4611fb7edcbf22638dafc29e5701a256918a
DESKTOP-CJB7SVG\shira <shirahata2808@gmail.com>
2021-06-20 23:10:47 +0900

    6/20

--- make_forder.bat
+++ make_forder.bat
@@ -2,9 +2,11 @@
 
 @REM 事前にコピー元のフォルダを作成しておく
 @REM 作成するフォルダのディレクトリに移動する
-cd agc
+cd abc
 
-@REM inの中に何番から何番のフォルダを作成するかを記載する
-for /l %%n in (10,1,99) do (
-    robocopy agcXXXX agc0%%n    
+@REM inの中に何番から何番のフォルダを作成するかを記載する in (開始値、増分、終了値)
+for /l %%n in (207,1,250) do (
+    robocopy abcXXXX abc%%n    
 )
+
+cd ..
