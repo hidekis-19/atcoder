8ce583cc8e8125d522c19bae32fe1f5f8f7d6c73
DESKTOP-CJB7SVG\shira <shirahata2808@gmail.com>
2021-06-26 20:40:06 +0900

    6/25

---
+++ others/joi2007本選/C_main.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+N = int(input())
+L = []
+
+
+for _ in range(N):
+    x,y = map(int,input().split())
+    L.append((x,y))
+L.sort(key=lambda x:(x[0], x[1]))
+L = set(L)
+ans = 0
+for p1,p2 in combinations(L,2):
+    p3 = (p1[0] + p2[1] - p1[1],p1[1] - p2[0] + p1[0])
+    p4 = (p2[0] + p2[1] - p1[1],p2[1] - p2[0] + p1[0])
+    if (p3 in L) and (p4 in L):
+        ans = max(ans,(p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+print(ans)
