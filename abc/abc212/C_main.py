1a676ebaf28fa5d0f2ecc096e10dbbe2b196493a
hideki shirahata <shirahata2808@gmail.com>
2021-08-01 19:37:25 +0900

    8/1

--- abc/abc212/C_main.py
+++ abc/abc212/C_main.py
@@ -1,11 +1,4 @@
-import numpy as np
-
-def getNearestValue(list, num):
-
-    # リスト要素と対象値の差分を計算し最小値のインデックスを取得
-    idx = np.abs(np.asarray(list) - num).argmin()
-    return list[idx]
-
+import bisect
 n,m = map(int,input().split())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
@@ -14,10 +7,14 @@ A = sorted(A)
 B = sorted(B)
 
 ans = 999999999999999999999
+y = 999999999999999999999
 
 for a in A:
-    b = getNearestValue(B,a)
-    ans = min(ans,abs(a-b))
+    s = bisect.bisect_left(B, a)
+    x = abs(a-B[s-1])
+    if s < m:
+        y = abs(a-B[s])
+    ans = min(ans,x,y)
 print(ans)
 
 
