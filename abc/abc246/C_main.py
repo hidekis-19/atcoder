8b7782c3e2f08aaf5547e804d2803b8df84542e3
DESKTOP-CJB7SVG\shira <shirahata2808@gmail.com>
2022-04-11 23:43:10 +0900

    4/11

--- abc/abc246/C_main.py
+++ abc/abc246/C_main.py
@@ -1,24 +1,20 @@
 n,k,x= map(int, input().split())
 A = list(map(int, input().split())) 
 
-
-B = []
-A = sorted(A,reverse=True)
-for i,a in enumerate(A) :
-    if a >=x:
-        B.append(a- x*(a//x))
-        k = k - a//x
-    else:
-        B.append(A[i+1:])
-        exit()
-print(A)
-B = sorted(B,reverse=True)
-print(B)
-
-if k ==0:
-    print(sum(B))
-else:
-    print(sum( B[k-1:]))
-            
-    
-    
+while True:
+    A = sorted(A,reverse=True)
+    s = 0
+    for i,a in enumerate(A):
+        s = i
+        if a < x:
+            break
+        elif a <= k*x:
+            k = k - a//x 
+            A[i] = a - (a//x)*x
+        else :
+            A[i] = a - k*x
+            print(sum(A))
+            exit()
+    if s==0:
+        break
+print(sum(A[k:]))
