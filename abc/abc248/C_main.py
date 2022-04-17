4a7e6e6a29b01c5b3ab1d1f79e2598d1b8262de2
DESKTOP-CJB7SVG\shira <shirahata2808@gmail.com>
2022-04-17 16:12:14 +0900

    4/17

--- abc/abc248/C_main.py
+++ abc/abc248/C_main.py
@@ -1,17 +1,10 @@
-n,m,k= map(int, input().split())
+n, m, K = list(map(int, input().split()))
+dp = [[0]*(K+1) for _ in range(n+1)]
+dp[0][0] = 1
 
-def get_integral_value_combination(list, target):
-    def a(idx, l, r, t):
-        if t == sum(l): r.append(l)
-        elif t < sum(l): return
-        for u in range(idx, len(list)):
-            a((u + 1), l + [list[u]], r, t)
-        return r
-    return a(0, [], [], target)
-
-list = []
-
-for i in range(1,n+1):
-    list.extend([i]*m)
-
-print(get_integral_value_combination(list, k))
+for i in range(n):
+    for j in range(K):
+        for k in range(1, m+1):
+            if (j+k<=K): 
+                dp[i+1][j+k] +=  dp[i][j]
+print(sum(dp[n]) % 998244353)
