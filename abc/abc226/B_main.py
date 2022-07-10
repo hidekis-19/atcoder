eab30343e3424fe0130a399f509a425f8f6e741e
DESKTOP-CJB7SVG\shira <shirahata2808@gmail.com>
2022-07-10 15:06:01 +0900

    7/10

--- abc/abc226/B_main.py
+++ abc/abc226/B_main.py
@@ -1,24 +1,7 @@
-from collections import deque
-from itertools import groupby
-def runLengthEncode(S: str) -> "List[tuple(str, int)]":
-    grouped = groupby(S)
-    res = []
-    for k, v in grouped:
-        res.append((k, int(len(list(v)))))
-    return res
-
-d = deque()
 n = int(input())
-
+A = set()
 for i in range(n):
-    x = list(map(int,input().split()))
-    x = runLengthEncode(x)
-    d.append(x)
-ans =0
-while d:
-    s = d.popleft()
-    if d.count(s)!=0:
-        d.remove(s)
-    ans +=1
-print(ans)
-    
+    A.add(input())
+print(len(A))    
+
+
